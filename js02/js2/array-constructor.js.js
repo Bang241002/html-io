7e35d4ed8c27fe85b8ced5f7934a2e10bdf92aa2
Bang241002 <91303671+Bang241002@users.noreply.github.com>
2021-11-15 15:23:40 +0700

    Create array-constructor.js.js

---
+++ js02/js2/array-constructor.js.js
@@ -0,0 +1,13 @@
+var usrname;
+var message;
+
+usrname = 'molly';
+message = 'see our upcoming range';
+
+var elname = document.getElementById('name');
+
+elname.textContent = usrname;
+
+var elnote = document.getElementById('note');
+
+elnote.textContent = message;
