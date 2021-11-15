d02425e0de4368917fd3dca37c64ec7d686705fb
Bang241002 <91303671+Bang241002@users.noreply.github.com>
2021-11-15 15:23:48 +0700

    Create numeric-variable.js.js

---
+++ js02/js2/numeric-variable.js.js
@@ -0,0 +1,13 @@
+var instock;
+var shipping;
+
+instock = true;
+shipping = false;
+
+var elstock = document.getElementById('stock');
+
+elstock.className = instock;
+
+var slship = document.getElementById('shipping');
+
+elship.className = shipping;
