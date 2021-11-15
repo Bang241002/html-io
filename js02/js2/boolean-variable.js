6a29f2de7f8a0edabd89f76499986d690f96fd79
Bang241002 <91303671+Bang241002@users.noreply.github.com>
2021-11-15 15:59:22 +0700

    Create boolean-variable.js

---
+++ js02/js2/boolean-variable.js
@@ -0,0 +1,12 @@
+var instock;
+var shipping;
+instock = true;
+shipping = false;
+
+var slstock = document.getElementById('stock');
+
+slstock.className = slstock;
+
+var elship = document.getElementById('shipping');
+
+elship.className = shipping;
