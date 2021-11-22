149afcf0ff002d4ea1d64e93f90ea51292d07671
Bang241002 <91303671+Bang241002@users.noreply.github.com>
2021-11-22 17:18:10 +0700

    Create for-loop.js

---
+++ bai13/js/for-loop.js
@@ -0,0 +1,14 @@
+var scores = [24, 32, 17];
+var arraylength = scores.length;
+var roundNumber = 0;
+var msg = '';
+
+for (var i = 0; i < arraylength; i++ ){
+
+    roundNumber = (i+1);
+
+    msg += 'Round' + roundNumber + ':';
+
+    msg += 'Round' + roundNumber + ':';
+}
+document.getElementById('answer').innerHTML = msg;
