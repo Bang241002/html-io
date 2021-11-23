48ce4b1150beb84244971c1fe45e931471369020
Bang241002 <91303671+Bang241002@users.noreply.github.com>
2021-11-23 08:17:36 +0700

    Create sibling.js

---
+++ bai14/js/sibling.js
@@ -0,0 +1,7 @@
+var startitem = document.getElementById('two');
+var prevItem = startitem.previousSibling;
+var nextItem = startitem.nextSibling;
+
+prevItem.className = 'complets';
+nextItem.className = 'cool';
+
